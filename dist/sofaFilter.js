979031f07cdc59bd45b9d64644949f91b6fea3ef
Robin Thrift <thrift.d.robin@googlemail.com>
2015-04-08 12:23:11 +0200

    chore: fixed build/cleanup

--- dist/sofaFilter.js
+++ dist/sofaFilter.js
@@ -1,3 +1,17 @@
+/**
+ * angular-sofa-filter - v0.1.0 - Wed Apr 08 2015 12:22:37 GMT+0200 (CEST)
+ * http://www.sofa.io
+ *
+ * Copyright (c) 2014 CouchCommerce GmbH (http://www.couchcommerce.com / http://www.sofa.io) and other contributors
+ * THIS SOFTWARE CONTAINS COMPONENTS OF THE SOFA.IO COUCHCOMMERCE SDK (WWW.SOFA.IO)
+ * IT IS PROVIDED UNDER THE LICENSE TERMS OF THE ATTACHED LICENSE.TXT.
+ */
+;(function (angular) {
+angular.module('sofa.filter', [
+    'sofa.filter.currency',
+    'sofa.filter.stringReplace'
+]);
+
 'use strict';
 angular
     .module('sofa.filter.currency', ['sofa.core'])
@@ -64,3 +78,46 @@ angular
             }
         };
     }]);
+
+angular
+    .module('sofa.filter.stringReplace', [])
+    .filter('stringReplace', [function () {
+
+        'use strict';
+        // Takes n arguments after "template". Either an array or arguments are turned into one
+        return function (template) {
+            var values = [];
+
+            if (arguments.length < 2) {
+                return template;
+            } else if (arguments.length > 2) {
+                // Turn all arguments except the first one into an array
+                values = Array.prototype.slice.call(arguments, 1);
+            } else {
+                var arg = arguments[1];
+
+                if (angular.isArray(arg)) {
+                    values = arg;
+                } else if (angular.isString(arg) || angular.isNumber(arg)) {
+                    values[0] = arg;
+                } else {
+                    return template;
+                }
+            }
+
+            var parse = function (template, values) {
+                var regEx = /%s/,
+                    hits  = template.match(/%s/g).length,
+                    i     = 0;
+
+                for (; i < hits + 1; i++) {
+                    template = template.replace(regEx, values[i]);
+                }
+
+                return template;
+            };
+
+            return parse(template, values);
+        };
+    }]);
+}(angular));
